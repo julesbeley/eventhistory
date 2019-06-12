f17293c3d95070a7dad07b54cb9ce2a31db3f71d
Jules Beley <jules.beley@gmail.com>
2019-06-12 13:13:30 +0200

    imported first dataset

--- day1.R
+++ day1.R
@@ -1,1 +1,12 @@
 library(survival)
+library(KMsurv)
+library(rio)
+agency <- import("./agency.dta")
+agency$enddate-agency$startdat # dur
+
+
+
+# to get rid of attributes
+for (i in (1:ncol(agency))) {
+    agency[,i] <- as.vector(agency[,i])
+}
