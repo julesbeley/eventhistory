86fbb4cd90bedec86215f76eb28f00e28867f49b
Jules Beley <jules.beley@gmail.com>
2019-06-12 16:11:05 +0200

    comments

--- day1.R
+++ day1.R
@@ -1,19 +1,34 @@
+# don't forget to install the packages before loading them
 library(survival)
 library(KMsurv)
 library(rio)
+
+# import stata dataset
 agency <- import("./agency.dta")
+
+# create censorship indicator
 agency <- cbind(agency, agency$enddate != "1997-12-31")
 colnames(agency)[100] <- "terminated"
+
+# convert logical to numeric
 agency$terminated <- as.numeric(agency$terminated)
+
+# number of agency terminations
 sum(agency$terminated[!is.na(agency$terminated)])
+
+# number of right-censored
 length(agency$terminated) - sum(agency$terminated)
 
+# creating a Surv object
 agencysurv <- Surv(agency$enddate - agency$startdat, 
                    event = agency$terminated)
+
+# fitting it 
 fit <- survfit(agencysurv~1)
 fit
 summary(fit)
-options(max.print = 5000)
+
+# plots
 png("KM.png", width = 800, height = 600)
 plot(fit, 
      main = "Survival of US government agencies",
