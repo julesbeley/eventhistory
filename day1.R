739ca29152dae5b9fccb18a7156699f435c0e2f6
Jules Beley <jules.beley@gmail.com>
2019-06-12 14:42:59 +0200

    created survival object

--- day1.R
+++ day1.R
@@ -2,11 +2,13 @@ library(survival)
 library(KMsurv)
 library(rio)
 agency <- import("./agency.dta")
-agency$enddate-agency$startdat # dur
-
-
-
-# to get rid of attributes
-for (i in (1:ncol(agency))) {
-    agency[,i] <- as.vector(agency[,i])
-}
+agency <- cbind(agency, agency$enddate != "1997-12-31")
+colnames(agency)[100] <- "terminated"
+agency$terminated <- as.numeric(agency$terminated)
+agencysurv <- Surv(agency$enddate - agency$startdat, event = agency$terminated)
+fit <- survfit(agencysurv~1)
+plot(fit)
+sum(agency$terminated[!is.na(agency$terminated)])
+table(agency$term)
+length(agency$terminated) - sum(agency$terminated)
+agencysurv
