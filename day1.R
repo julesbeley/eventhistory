7717b4a9941ec14a840a55f24ec95b74a3f1900d
Jules Beley <jules.beley@gmail.com>
2019-06-12 16:07:42 +0200

    log rank test and various plots

--- day1.R
+++ day1.R
@@ -5,10 +5,42 @@ agency <- import("./agency.dta")
 agency <- cbind(agency, agency$enddate != "1997-12-31")
 colnames(agency)[100] <- "terminated"
 agency$terminated <- as.numeric(agency$terminated)
-agencysurv <- Surv(agency$enddate - agency$startdat, event = agency$terminated)
-fit <- survfit(agencysurv~1)
-plot(fit)
 sum(agency$terminated[!is.na(agency$terminated)])
-table(agency$term)
 length(agency$terminated) - sum(agency$terminated)
-agencysurv
+
+agencysurv <- Surv(agency$enddate - agency$startdat, 
+                   event = agency$terminated)
+fit <- survfit(agencysurv~1)
+fit
+summary(fit)
+options(max.print = 5000)
+png("KM.png", width = 800, height = 600)
+plot(fit, 
+     main = "Survival of US government agencies",
+     xlab = "Time in days",
+     ylab = "Probability of surviving (Kaplan-Meier)")
+dev.off()
+
+hazard <- -log(fit$surv)
+png("H.png", width = 800, height = 600)
+plot(fit$time, 
+     hazard, 
+     type = "s",
+     main = "Hazard function for US government agencies",
+     xlab = "Time in days",
+     ylab = "Hazard function")
+dev.off()
+
+fitleg <- survfit(agencysurv~leg)
+png("leg.png", width = 800, height = 600)
+plot(fitleg, 
+     main = "Survival of US government agencies",
+     xlab = "Time in days",
+     ylab = "Probability of surviving (Kaplan-Meier)")
+text(x = 15000, y = 0.5, "leg = 1")
+text(x = 9000, y = 0.2, "leg = 0")
+dev.off()
+fitleg
+
+# log rank test (sts test)
+survdiff(Surv(agency$enddate - agency$startdat, event = agency$terminated) ~ leg)
